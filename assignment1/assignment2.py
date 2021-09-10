9ca54511cdb4e05c003b6d302653b7a7b2657844
Evgeny Barykin <evgeny.barykin@irdeto.com>
2021-09-10 10:17:00 +0200

    Old files moved

--- assignment1/assignment2.py
+++ assignment1/assignment2.py
@@ -40,7 +40,14 @@ if __name__ == '__main__':
         # Execute MapReduce job in parallel.
         map_reduce = MapReduce(map_lines_to_words, reduce_word_count, 8)
         date_counts = map_reduce(tracks, debug=True)
-        # Print result
         print('Songs played on ' + str(MONTH) + '/'+ str(YEAR) )
         for track_id, played_times in date_counts:
             print("Song id " + track_id + ", played times:"+ str(played_times))
+    # # Sort the result in reverse order
+    # word_counts.sort(key=operator.itemgetter(1))
+    # word_counts.reverse()
+    # top20 = word_counts[:20]
+    
+    # print('Top 20 words by frequency:')
+    # for word, count in top20:
+    #     print('{:10s}: {}'.format(word, count))
