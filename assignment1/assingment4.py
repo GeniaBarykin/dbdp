a5a7a24a08bcc7ab6134ddc15ea53e6dd87a3a1b
Evgeny Barykin <evgeny.barykin@irdeto.com>
2021-09-17 11:35:26 +0200

    ass1.4 fixed

--- assignment1/assingment4.py
+++ assignment1/assingment4.py
@@ -8,36 +8,45 @@ TRACKS_FILEPATH = "data/tracks.csv"
 
 #1
 def mapper_track_to_users_and_authors(line):
-    output = []
     if len(line) == 3:
         track_id,user_id,datetime=line        
-        output.append((track_id, int(user_id)))
+        return [(track_id, int(user_id))]
     elif len(line) == 4:
         track_id,artist,title,lengthSeconds=line
-        output.append((track_id, artist))   
-        
-    return output
+        return [(track_id, artist)] 
+    elif len(line) == 7:
+        user_id,first_name,last_name,email,gender,country,dob=line
+        return [(int(user_id), (first_name, last_name))]
 
 def reducer_authors_to_users(key_value_item):
-    track_id, data = key_value_item 
-    users_listened_author=[]
-    author = None
-    for element in data:           
-        if isinstance(element, int): 
-            users_listened_author.append(element)
-        if isinstance(element, str):
-            author=element
-    return (author,users_listened_author)
+    some_id, data = key_value_item 
+    output = None    
+    users_listened_author = []
+    if isinstance(some_id, str):    
+        for element in data:           
+            if isinstance(element, int): 
+                users_listened_author.append(element)
+            if isinstance(element, str):
+                author=element
+        return (author,users_listened_author)
+    elif isinstance(some_id, int): 
+        return (some_id, data.pop(0))
+    
 #2
 def mapper_user_to_author(line):
-    author, users = line
+    some_id, data = line
     output = []
-    for user in users:
-        output.append((user, author))
+    if isinstance(some_id, str): 
+        for user in data :
+            output.append((user, some_id))
+    elif isinstance(some_id, int):
+        output.append((some_id, data))
     return output
 
 def reducer_user_fav_author(key_value_item):
-    user, authors = key_value_item
+    user_id, data = key_value_item
+    initials = data.pop(0)
+    authors = data
     listened_authors_count={}
     for author in authors:
         if author in listened_authors_count:
@@ -50,46 +59,26 @@ def reducer_user_fav_author(key_value_item):
         if listened_authors_count[author] > times_listened:
             fav_author= author
             times_listened = listened_authors_count[author]
-    return (user, fav_author, times_listened)
-
-#3
-def mapper_user_to_info(line):
-    output = []  
-    if len(line) == 3:
-        user_id, author, times_listened = line      
-        output.append((user_id, (author, times_listened)))             
-    elif len(line) == 7:
-        user_id,first_name,last_name,email,gender,country,dob=line
-        output.append((int(user_id), (first_name, last_name))) 
-    return output
-
-def reducer_user_info(key_value_item): 
-    output = []
-    user_id, data = key_value_item
-    output.append({"fist_name" : data[1][0],"second_name" : data[1][1], "fav_author" : data[0][0],"times_listened" : data[0][1]})
-    return output
-
+    return (initials[0], initials[1], fav_author, times_listened)
+    
 if __name__ == '__main__':
     data=[]
     # Read data into separate lines.
     with open (HISTORY_FILEPATH, 'r') as history_input_file, open (TRACKS_FILEPATH, 'r', encoding="UTF-8") as tracks_input_file:
-        history_reader = csv.reader(history_input_file, delimiter=',')
-        next(history_reader, None)  # skip the headers
-        data =list(history_reader)        
-        tracks_reader = csv.reader(tracks_input_file, delimiter=',')
-        next(tracks_reader, None)  # skip the headers
-        data+=list(tracks_reader)            
-        # # Execute MapReduce job in parallel.
-        map_reduce = MapReduce(mapper_track_to_users_and_authors, reducer_authors_to_users, 8)
-        data = map_reduce(data, debug=True)
-        map_reduce = MapReduce(mapper_user_to_author, reducer_user_fav_author, 8)
-        data = map_reduce(data, debug=True)
         with open (PEOPLE_FILEPATH, 'r') as people_input_file:
             people_reader = csv.reader(people_input_file, delimiter=',')
             next(people_reader, None)  # skip the headers
-            
-            data+=list(people_reader)
-            map_reduce = MapReduce(mapper_user_to_info, reducer_user_info, 8)
+            data =list(people_reader)
+            history_reader = csv.reader(history_input_file, delimiter=',')
+            next(history_reader, None)  # skip the headers
+            data +=list(history_reader)        
+            tracks_reader = csv.reader(tracks_input_file, delimiter=',')
+            next(tracks_reader, None)  # skip the headers
+            data +=list(tracks_reader)            
+            # # Execute MapReduce job in parallel.
+            map_reduce = MapReduce(mapper_track_to_users_and_authors, reducer_authors_to_users, 8)
+            data = map_reduce(data, debug=True)
+            map_reduce = MapReduce(mapper_user_to_author, reducer_user_fav_author, 8)
             data = map_reduce(data, debug=True)
     for line in data:
         print(line)
