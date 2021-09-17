88b66c15f65513ab10e55e67be516abc89612ebf
victorhubbers <469487@student.saxion.nl>
2021-09-17 12:25:27 +0200

    assignment 3.1

--- assignment3/assignment1.py
+++ assignment3/assignment1.py
@@ -2,23 +2,31 @@ import csv
 from datetime import datetime as dt
 from map_reduce_lib import MapReduce
 
-PATH = "access_log/access_log-test"
+PATH = "access_log/access_log"
+
 
 def mapper(line):
-    output=[]
-    print(line)
-    return output
+    output = []
+    
+    ip_address = line.split(" ")[0]
+    output.append((ip_address, 1))
+
+    return output 
+
 
 def reducer(key_value_item):
-    output=[]
-    return output
+    ip_address, occurances = key_value_item
+    return (ip_address, sum(occurances))
 
 if __name__ == '__main__':
-    data=[]
+    data = []
     with open(PATH, 'r') as f:
-        reader = csv.reader(f,delimiter=' ')
-        data=list(reader)
-        map_reduce = MapReduce(mapper, reducer, 8)
-        data = map_reduce(data, debug=True)
-            
+        lines = f.readlines()
 
+        map_reduce = MapReduce(mapper, reducer, 8)
+        data = map_reduce(lines, debug=True)
+
+
+
+    for ip_address, visits in data:
+        print(f"{ip_address} visited {visits} times.")
